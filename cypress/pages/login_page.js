323f61090e1fcd0286a4e0eee75eb271392d8171
Arsal Syed <arsal.syed@teo-intl.com>
2026-05-01 18:12:16 +0500

    Step 1

--- cypress/pages/login_page.js
+++ cypress/pages/login_page.js
@@ -1,18 +1,8 @@
 // This file contains the page object for the login page. The login page object contains the following methods:
 class LoginPage {
-  visit() {
+   visit() {
     const baseUrl = Cypress.env("BASE_URL") || '/';
-    return cy.visit(baseUrl, {
-      onBeforeLoad(win) {
-        const origOpen = win.XMLHttpRequest.prototype.open;
-        win.XMLHttpRequest.prototype.open = function(method, url, async, user, password) {
-          if (async === false) {
-            async = true;
-          }
-          return origOpen.call(this, method, url, async, user, password);
-        };
-      }
-    });
+    return cy.visit(baseUrl);
   }
 }
 
